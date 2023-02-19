46684f7122ebd6761022ff0b1a96f3a8acfdfce1
Azat <aelchikushuulu@gmail.com>
2023-02-19 16:40:38 +0600

    Task6

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
     }
 
     public static void task1 () {
@@ -92,6 +93,20 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+    }
+
+    public static void task6 () {
+
+        System.out.println("Задача 6");
+
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+
+        var totalWeightOfBoxers = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес двух бойцов " + totalWeightOfBoxers + " кг!");
+
+        var differenceWeight = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница между весами бойцов состовляет " + differenceWeight + " кг!");
     }
 
 }
