b92226b08c740aa1faba3e269b262e0a4ee5cf62
Azat <aelchikushuulu@gmail.com>
2023-02-19 16:14:32 +0600

    Innitial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1 () {
+
+        System.out.println("Задача 1");
+
+        var dog = 8.0;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
+    }
+
+}
